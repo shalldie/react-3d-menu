eb3bffd89f875c54fd0fd398a37dddb96e7496fd
xieshuang <hi.xieshuang@gmail.com>
2024-06-13 18:41:21 +0800

    finish demo buld、prepublish

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,44 @@
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+console.log(process.env.BUILD_DEMO);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        //
-        react(),
-        cssInjectedByJsPlugin(),
-        dts()
-    ],
-    build: {
-        lib: {
-            entry: path.join(__dirname, 'src/react-3d-menu.tsx'),
-            name: 'React3DMenu',
-            fileName: 'react-3d-menu'
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM'
+export default defineConfig(config => {
+    const isDemo = config.mode === 'demo';
+
+    return {
+        plugins: [
+            //
+            react(),
+            ...(() => {
+                if (isDemo) {
+                    return [viteSingleFile()];
                 }
-            }
-        }
-    }
+                return [cssInjectedByJsPlugin(), dts()];
+            })()
+        ],
+        build: isDemo
+            ? undefined
+            : {
+                  lib: {
+                      entry: path.join(__dirname, 'src/react-3d-menu.tsx'),
+                      name: 'React3DMenu',
+                      fileName: 'react-3d-menu'
+                  },
+                  rollupOptions: {
+                      external: ['react', 'react-dom'],
+                      output: {
+                          globals: {
+                              react: 'React',
+                              'react-dom': 'ReactDOM'
+                          }
+                      }
+                  }
+              }
+    };
 });
