5c26778e7e881a3e2668f460a9bb882caf4ec544
xieshuang <hi.xieshuang@gmail.com>
2024-06-18 11:19:40 +0800

    update demo

--- src/demos/index.tsx
+++ src/demos/index.tsx
@@ -3,9 +3,9 @@ import { MenuDemo1, MenuDemo2, MenuDemo3 } from './demos';
 export function App() {
     return (
         <>
-            <h4>
-                <a target="_blank" href="https://github.com/shalldie/react-3d-menu" style={{ paddingLeft: 20 }}>
-                    Github - 3D menu component for React.
+            <h4 style={{ paddingLeft: 20 }}>
+                <a target="_blank" href="https://github.com/shalldie/react-3d-menu">
+                    Github - React's 3D menu component. React 3D 菜单组件。
                 </a>
             </h4>
             <div className="app">
